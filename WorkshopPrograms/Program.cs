859e23cf43f431277ccfadf87ecab9672effdbf2
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-17 23:40:04 +0530

    (Shubham)Add: Converting temperature to other units

--- WorkshopPrograms/Program.cs
+++ WorkshopPrograms/Program.cs
@@ -5,7 +5,33 @@ namespace WorkshopPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Workshop Programs");
+            Console.WriteLine("Welcome to the Workshop Programs\n");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select from the following\n1.Temperatureconversion\n2.Exit");
+                Console.Write("Enter your choice: ");
+                try
+                {
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            TemperatureConversion convert = new TemperatureConversion();
+                            convert.TempConversion(100);
+                            Console.WriteLine("--------------------------------------\n");
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
         }
     }
 }
---
+++ WorkshopPrograms/TemperatureConversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace WorkshopPrograms
+{
+    public class TemperatureConversion
+    {
+        public void TempConversion(int temp)
+        {
+            Console.WriteLine("1.Convert Celsius to Fahrenheit\n2.Convert Fahrenheit to Celsius");
+            try
+            {
+                int select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        float fahr = (temp * (9 / 5f)) + 32;
+                        Console.WriteLine("{0}C into Fahrenheit is {1}F", temp, fahr);
+                        break;
+                    case 2:
+                        float cels = (temp - 32) * (5 / 9f);
+                        Console.WriteLine("{0}F into Celsius is {1}C", temp, cels);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
